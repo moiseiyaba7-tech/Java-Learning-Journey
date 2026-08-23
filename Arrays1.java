0fdb267c8fbe68b54ad170fac4015bcc4ac733d4
Moise iyaba <moiseiyaba7@gmail.com>
2026-08-24 00:28:49 +0300

    Create Arrays1.java

---
+++ Arrays1.java
@@ -0,0 +1,17 @@
+public class Arrays1 {
+  public static void main(String[] args) {
+
+    //Another way to write an array, along with a for loop
+    //Elements : before"Charger", "Pen", "Book", "Phone"
+
+    String [] Bag = new String[4];
+    Bag[0] = "Charger";
+    Bag[1] = "Pen";
+    Bag[2] = "Book";
+    Bag[3] = "Phone";
+
+    for(int i=0; i<=5; i++){
+     System.out.println(Bag[1]);
+    }
+  }
+}
